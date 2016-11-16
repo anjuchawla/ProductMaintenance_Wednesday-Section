b738a7a1da874ed25402b079e5cc22c35ab2101d
Anju Chawla <anju.chawla@georgiancollege.ca>
2016-11-16 10:15:58 -0500

    Added the validator class

--- ProductMaintenance/Validator.cs
+++ ProductMaintenance/Validator.cs
@@ -1,3 +1,9 @@
+/*
+ * Name: Anju Chawla
+ * Date: November 2016
+ * Purpose: A helper class that validates data
+ * */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +15,55 @@ namespace ProductMaintenance
 {
     public static class Validator
 	{
-		
+        private static string title = "Entry Error";
+
+        public static string Title { get; set; }
+
+        //this method checks if a text box is empty
+        public static Boolean IsPresent(TextBox textBox)
+        {
+            if(textBox.Text == String.Empty)
+            {
+                MessageBox.Show(textBox.Tag + " is a required field",
+                    Title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+            return true;
+        }
+
+        public static Boolean IsDecimal(TextBox textBox)
+        {
+            decimal number = 0m;
+            if (Decimal.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(textBox.Tag + " must be a decimal value", Title,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.SelectAll(); 
+                textBox.Focus();
+                return false;
+            }
+        }//IsDecimal
+
+        public static Boolean IsWithinRange(TextBox textBox, decimal min, decimal max)
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if(number < min || number > max)
+            {
+                MessageBox.Show(textBox.Tag + " must be between " + min + " and " + max,
+                    Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.SelectAll();
+                textBox.Focus();
+
+                return false;
+
+            }
+            return true;
+        }
+
             
 	}
 }
--- ProductMaintenance/frmProductMain.cs
+++ ProductMaintenance/frmProductMain.cs
@@ -29,13 +29,14 @@ namespace ProductMaintenance
 
 		private void frmProductMain_Load(object sender, EventArgs e)
 		{
-            //populate the list with products
+            //populate the list with products 
             products = ProductDB.GetProducts();
             FillProductListBox();			
 		}
 
         private void FillProductListBox()
         {
+            //populates the list box with the products
             lstProducts.Items.Clear();
             foreach(Product p in products)
             {
@@ -46,11 +47,52 @@ namespace ProductMaintenance
 
         private void btnAdd_Click(object sender, EventArgs e)
 		{
+            //the form to add new product is displayed
+            frmNewProduct newProductForm = new frmNewProduct();
+            newProductForm.Show(); 
+          /* Product product = newProductForm.GetNewProduct();
+
+            if(product != null)
+            {
+                products.Add(product);
+                ProductDB.SaveProducts(products);
+                FillProductListBox();
+            }
+            */
 			
 		}
 
 		private void btnDelete_Click(object sender, EventArgs e)
 		{
+            //delete a product
+            int i = lstProducts.SelectedIndex;
+
+            if(i != -1) //user has selected item to delete
+            {
+                Product product = products[i];
+                string message = "Are you sure you want to delete the product";
+
+                DialogResult confirm = MessageBox.Show(message, "Confirm Deletion",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button2);
+
+                if(confirm == DialogResult.Yes )
+                {
+                    products.Remove(product);
+                    ProductDB.SaveProducts(products);
+                    FillProductListBox();
+                }
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a product to delete", 
+                    "Delete Unsuccessful", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Exclamation);
+            }
+
 			
 		}
 
